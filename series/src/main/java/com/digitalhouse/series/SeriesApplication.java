b81d74027b122d543e9c0570bc89972d030134d9
Anderson Ribeiro <andersonsiilvar@gmail.com>
2022-10-04 09:34:47 -0300

    Update

--- series/src/main/java/com/digitalhouse/series/SeriesApplication.java
+++ series/src/main/java/com/digitalhouse/series/SeriesApplication.java
@@ -2,15 +2,20 @@ package com.digitalhouse.series;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-
 public class SeriesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SeriesApplication.class, args);
 	}
-
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+		return restTemplateBuilder.build();
+	}
 }
